f032a0b5190b5fbcf6ffe88664b492eeb04f8ac6
Prinda-codes <prinda.codes@gmail.com>
2025-07-30 17:51:15 +0530

    .env added and axios changed

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api/v1';
 
 export default axios.create({
-    baseURL:'http://localhost:8080/api/v1',
-    headers:{
-        'Content-Type':'application/json'
-    }
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
